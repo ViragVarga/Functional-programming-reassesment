a874b9958d2f70400bcb7ee6f154becad4db00f2
ViragVarga <varga.virag.jenny@gmail.com>
2025-09-26 16:57:47 +0100

    Updated .gitignore and got rid of practice assignments folder

--- Practice_assignments/deep0401.hs
+++
@@ -1,7 +0,0 @@
-newtype Expr = Expr { eval :: Int}
-
-constant :: Int -> Expr
-constant n = Expr n
-
-add :: Expr -> Expr -> Expr
-add e1 e2 = Expr (eval e1 + eval e2)
--- Practice_assignments/kcs.hs
+++
@@ -1,9 +0,0 @@
-rotatingSquare :: Signal Drawing
-rotatingSquare = fmap (:[]) $ fmap sq rs
-                    where
-                        rs :: Signal Transform
-                        rs = fmap rotate timeS
-                        
-                        sq :: Transform -> (Transform, Shape)
-                        sq t = ( scale (point 0.5 0.5) <+>
-                                translate (point 1.2 0.4) <+> t, square)
